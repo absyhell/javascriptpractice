b8a3f7f5544440087c8bca8f2858e13408190642
Ayush Raj <148205492+absyhell@users.noreply.github.com>
2026-02-18 14:14:53 +0000

    learned aboutr it datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"//newer version of js bas
+// alert("hello")old syntax we are using node js not browser console
+//original documentation for java script is ecma script
+
+
+
+//types of data types
+/*number
+string
+bigint
+boolean
+null=it is a datatype and also standalone value
+undefined =variable declare kara hai but koi value assign nhi ki hai 
+symbol jab react padenge tab iska use karenge for differentiating stuffs from one another
+*/
+/*object arrays fn are complex or non primitive data type
+*/
+let a=123456789987654321234567897654345678765432345678909876543456789876543456789123456789098765432345678998765432345678765432345678765432345678765432345678987654323456789876543345678987654345678n
+console.log(typeof("xyz"))
+console.log(typeof(a));
